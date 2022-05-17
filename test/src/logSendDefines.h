8e729d152c574f45673a3397a983988201564734
AppelEater <david@vajo.dk>
2022-05-17 13:46:12 +0200

    Forgot the Defines File

---
+++ test/src/logSendDefines.h
@@ -0,0 +1,15 @@
+
+/* The different defines that specify the code send to the LOG subsystem*/
+
+/* When starting, this code i called in main before the task scheduler is started*/
+#define bootUpInMain 0
+
+/* Log message sent when the module starts taking a picture*/
+#define begunTakingPicture 1
+
+/* Log message sent when the module is done taking picture and compressing it*/
+#define doneTakingPicture 2
+
+
+
+
